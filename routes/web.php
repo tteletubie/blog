9808ac259002c33a8827cca0295cdd84543e07f3
Kyb3r Cipher <me@kyb3rcipher.com>
2024-10-29 21:28:27 -0600

    Colocar vista home en el index

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function() {
+Route::get('/', function() {
     return view('home');
 });
